fd1222e0c3d038655cfbd073ef25b15eefdf6ef7
Deni Chan <dchan@yahoo.com>
2018-08-24 13:50:25 +0300

    fixed path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path');
 const app = express()
-const port = 3001
+const port = 8080
 
 app.get('/api/get_word', (request, response) => {
   fs.readFile('./words.txt', 'utf-8', function(err, data) {
